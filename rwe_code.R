a3bd55f0665a1da2f59efcab044ef2e2d76fdccc
seantok <seantok1990@gmail.com>
2024-05-02 09:46:32 +0200

    Final draft of code. Beginning on presentation.

--- rwe_code.R
+++ rwe_code.R
@@ -12,6 +12,7 @@ library(tidyverse)
 library(ggplot2)
 library(naniar)
 library(lubridate)
+library(corrplot)
 
 #Get working directory of the script
 dir_name=dirname(rstudioapi::getSourceEditorContext()$path)
@@ -40,7 +41,7 @@ patient_data=full_join(as.data.frame(clinical_data),as.data.frame(demographic_da
 #Seems there are multiple bills associated with a single patient. Perhaps best to 
 #calculate summarized billing data
 billing_data=full_join(billing_amt_data,billing_id_data,'bill_id')
-billing_data=billing_data %>%  group_by(patient_id) %>% summarize(mean=mean(amount),min=min(amount),max=max(amount),count=n())
+billing_data=billing_data %>%  group_by(patient_id) %>% summarize(bill_mean=mean(amount),bill_min=min(amount),bill_max=max(amount),bill_count=n())
 
 #perform full join on patient data and billing data
 full_dataset=full_join(patient_data,billing_data,'patient_id') %>% as.data.frame()
@@ -74,23 +75,45 @@ mcar_test(full_dataset)
 #Propose to remove the rows with missing values since value does not seem to high
 trimmed_dataset=na.omit(full_dataset)
 
-#convert columns to factors
-trimmed_data=trimmed_dataset %>% mutate(across(matches('trt|symp|race|gender|resident'),as.factor))
+#rename trimmed dataset as dataset for easy swapping if need be
+dataset=trimmed_dataset
 
 #Perform feature generation on current features
 #Generate age, duration of stay as well as change in CGIS score at discharge, number of co-occuring symptoms and treatments
-trimmed_dataset= trimmed_dataset %>% mutate(age=floor(as.integer(dmy(.$date_of_admission)-ymd(.$date_of_birth))/365))
-trimmed_dataset= trimmed_dataset %>% mutate(stay_duration=as.integer(dmy(.$date_of_discharge)-dmy(.$date_of_admission)))
-trimmed_dataset= trimmed_dataset %>% mutate(cgis_diff=.$cgis_dis-.$cgis_adm)
-trimmed_dataset$coocur_symp= trimmed_dataset %>% select(matches('symptom')) %>% rowSums()
-trimmed_dataset$coocur_trt= trimmed_dataset %>% select(matches('trt')) %>% rowSums()
+dataset= dataset %>% mutate(age=floor(as.integer(dmy(.$date_of_admission)-ymd(.$date_of_birth))/365))
+dataset= dataset %>% mutate(stay_duration=as.integer(dmy(.$date_of_discharge)-dmy(.$date_of_admission)))
+dataset= dataset %>% mutate(cgis_diff=.$cgis_dis-.$cgis_adm)
+dataset$coocur_symp= dataset %>% select(matches('symptom')) %>% rowSums()
+dataset$coocur_trt= dataset %>% select(matches('trt')) %>% rowSums()
 
 #Generate descriptive statistics
-age_summary= trimmed_dataset %>% summarise(mean=mean(age),median=median(age),min=min(age),max=max(age),sd=sd(age))
-symptom_summary=trimmed_dataset %>% select(matches('symptom')) %>% summarise_all(sum)/nrow(trimmed_dataset)*100
-coccur_symptoms=table(trimmed_dataset$coocur_symp)/nrow(trimmed_dataset)*100
-treatment_summary=trimmed_dataset %>% select(matches('trt')) %>% summarise_all(sum)/nrow(trimmed_dataset)*100
-coccur_treat=table(trimmed_dataset$coocur_trt)/nrow(trimmed_dataset)*100
-gender_info=table(trimmed_dataset$gender)/nrow(trimmed_dataset)*100
-race_info=table(trimmed_dataset$race)/nrow(trimmed_dataset)*100
-resident_info=table(trimmed_dataset$resident_status)/nrow(trimmed_dataset)*100
+age_summary= dataset %>% summarise(mean=mean(age),median=median(age),min=min(age),max=max(age),sd=sd(age))
+symptom_summary=dataset %>% select(matches('symptom')) %>% summarise_all(sum)/nrow(dataset)*100
+coccur_symptoms=table(dataset$coocur_symp)/nrow(dataset)*100
+treatment_summary=dataset %>% select(matches('trt')) %>% summarise_all(sum)/nrow(dataset)*100
+coccur_treat=table(dataset$coocur_trt)/nrow(dataset)*100
+gender_info=table(dataset$gender)/nrow(dataset)*100
+race_info=table(dataset$race)/nrow(dataset)*100
+resident_info=table(dataset$resident_status)/nrow(dataset)*100
+
+#generate distribution graphs
+hist(dataset$age,breaks = seq(20,90,2))
+
+#convert columns to factors
+dataset=dataset %>% mutate(across(matches('trt_|symptom|race|gender|resident'),as.factor))
+
+#Main study question and analysis: which factors influence treatment outcomes?
+#We can take the CGIS score of the patient at admission as baseline and a
+#successful treatment as a score that is lower than the baseline. In this sense
+#you can binarize the outcome and run a binomial regression to determine which
+#factors may influence the outcome.
+
+dataset$outcome[dataset$cgis_diff<0]=1
+dataset$outcome[dataset$cgis_diff>=0]=0
+
+#select relevant variables for regression
+stat_data=dataset %>% select(matches('trt|symp|age|weight|height|medical|gaf|gender|race|outcome'))
+cgis_glm=glm(outcome~.,data = stat_data,family = 'binomial')
+cgis_summ=summary(cgis_glm)
+
+#NAs returned. Collinearity? Requires more inspection
